246b35054a159b4219d13712ad131a35ef0fd0de
fanzdecul887-cmd <fanzdecul887@gmail.com>
2026-05-29 02:05:27 +0900

    Add visual effects functions for gameplay

---
+++ scripts/vfx.gd
@@ -0,0 +1,11 @@
+extends Node
+
+func play_slash_effect(position):
+	print("⚔ Slash Effect at:", position)
+	# nanti diganti particle / animation
+
+func play_aura():
+	print("✨ Excalibur Aura Activated")
+
+func screen_shake():
+	print("💥 Screen Shake!")
