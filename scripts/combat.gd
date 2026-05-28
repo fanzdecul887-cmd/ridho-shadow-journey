5e9757a4663f10f304c0cb148364e0b023708f5b
fanzdecul887-cmd <fanzdecul887@gmail.com>
2026-05-29 02:00:02 +0900

    Update damage calculation and combo reset logic
    Refactor damage calculation and combo handling in combat script.

--- scripts/combat.gd
+++ scripts/combat.gd
@@ -1,21 +1,31 @@
 extends Node
 
-var damage = 25
+var base_damage = 25
 var combo = 0
+var combo_timer = 0.0
 var combo_reset_time = 1.2
+
 var can_attack = true
+
+func _process(delta):
+	if combo > 0:
+		combo_timer += delta
+		if combo_timer >= combo_reset_time:
+			combo = 0
+			combo_timer = 0
 
 func attack():
 	if not can_attack:
 		return
 
 	can_attack = false
-	combo += 1
 
-	print("Combo:", combo, "Damage:", damage)
+	combo += 1
+	combo_timer = 0
+
+	var final_damage = base_damage + (combo * 5)
+
+	print("⚔ Attack! Combo:", combo, "Damage:", final_damage)
 
 	await get_tree().create_timer(0.4).timeout
 	can_attack = true
-
-	await get_tree().create_timer(combo_reset_time).timeout
-	combo = 0
